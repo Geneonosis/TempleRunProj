5fcbc600f9ad677371b42dc9787adfe36c745168
Gene Tigner <genetigner.art@gmail.com>
2019-09-28 02:11:54 -0500

    Death Manager
    Created a death manager to force the game to reload the scene whenever the player dies

---
+++ Assets/Scripts/DeathManager.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class DeathManager : MonoBehaviour
+{
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.tag.Equals("Player"))
+        {
+            //in theory should reset the game if i die
+
+            SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+        }
+    }
+}

ee1981054b2953fb6db86bbf112c76feb885a083
Gene Tigner <genetigner.art@gmail.com>
2019-09-28 10:06:24 -0500

    Clock Fix
    Clock was jittery and moved around alot, fixed the clock so that it stayed stationary when counting time.

--- Assets/ClockScript.cs
+++ Assets/ClockScript.cs
@@ -6,29 +6,47 @@ using TMPro;
 
 public class ClockScript : MonoBehaviour
 {
-    private TextMeshProUGUI _clockTMProComponent;
+    [SerializeField] private TextMeshProUGUI _clockTMProSeconds;
+    [SerializeField] private TextMeshProUGUI _clockTMProMilliSeconds;
     private int _minutes;
-    private float _seconds;
+    private int _seconds;
     private float _tenthASecond;
     [SerializeField] private float _timeLimit = 30;
     // Start is called before the first frame update
 
+    [SerializeField] private TextMeshProUGUI levelText;
+    private int levelNumber = 1;
+    private bool key = false;
     void Start()
     {
-        _clockTMProComponent = gameObject.GetComponent<TextMeshProUGUI>();
+        //_clockTMProSeconds = gameObject.GetComponent<TextMeshProUGUI>();
         _minutes = 0;
         _seconds = 0;
         _tenthASecond = 0;
+        levelText.SetText(levelNumber.ToString("Level: 00"));
     }
 
     // Update is called once per frame
     void Update()
     {
 
-        _seconds = _timeLimit - (Time.time % _timeLimit);
+        _seconds = (int)(_timeLimit - (Time.timeSinceLevelLoad % _timeLimit));
 
-        _clockTMProComponent.SetText(_seconds.ToString("0:00.<size=45>00</size>"));
-        //_clockTMProComponent.SetSecondClockText(Time.time);
+        _clockTMProSeconds.SetText(_seconds.ToString("00"));
+        _clockTMProMilliSeconds.SetText(_tenthASecond.ToString("00"));
         
+        if((int)_seconds == 0 && key == false)
+        {
+            key = true;
+            levelNumber++;
+            levelText.SetText(levelNumber.ToString("Level: 00"));
+            //show some sort of indication
+        }
+
+        if((int)_seconds == 1)
+        {
+            key = false;
+        }
+        //_clockTMProComponent.SetSecondClockText(Time.time);
     }
 }
